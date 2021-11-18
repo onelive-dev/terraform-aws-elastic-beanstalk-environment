25382f837a36de825c5d6cdf0703f0a1b1257578
Clayton Dittman <clayton.dittman@xdelivery.ai>
2021-11-18 12:14:06 -0600

    refine resource label with regex and data sources

--- autoscaling.tf
+++ autoscaling.tf
@@ -9,9 +9,22 @@ resource "aws_autoscaling_policy" "target_tracking" {
   target_tracking_configuration {
     predefined_metric_specification {
       predefined_metric_type = var.tt_metric_type
-      resource_label = "elasticbeanstalk:environment-name:${var.name}"
+      resource_label = "app/${regex(data.aws_lb.eb-alb.arn, "\\/awseb\\-AWSEB-[a-zA-Z0-9]+\\/[a-zA-Z0-9]+")}/${regex(data.aws_resourcegroupstaggingapi_resources.eb-alb-tg.resource_tag_mapping_list.0.resource_arn, "\\/awseb\\-AWSEB-[a-zA-Z0-9]+\\/[a-zA-Z0-9]+")}"
     }
     target_value     = var.tt_target_value
     disable_scale_in = var.tt_disable_scale_in
+  }
+}
+
+data "aws_lb" "eb-alb" {
+  arn  = aws_elastic_beanstalk_environment.default.load_balancers[0]
+}
+
+# Regex \/awseb\-AWSEB-[a-zA-Z0-9]+\/[a-zA-Z0-9]+
+data "aws_resourcegroupstaggingapi_resources" "eb-alb-tg" {
+  resource_type_filters = ["elasticloadbalancingv2:targetgroup"]
+  tag_filter {
+    key    = "elasticbeanstalk:environment-name"
+    values = ["${var.name}",]
   }
 }
